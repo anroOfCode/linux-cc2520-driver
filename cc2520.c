845ebd4b81f0cbe32e838c7187a0c907ed86de74
Andrew Robinson <andrew.robinson@gmail.com>
2012-12-19 15:27:07 -0500

    refactored gpio init code

--- cc2520.c
+++ cc2520.c
@@ -11,15 +11,28 @@
 #define DRIVER_AUTHOR "Andrew Robinson <androbin@umich.edu>"
 #define DRIVER_DESC   "A driver for the CC2520 radio. Be afraid."
 
+struct cc2520_state state;
 
 int irqNumber = 0;
 int pinValue = 0;
 
 struct hrtimer utimer;
 
+//////////////////////////
+// Interrupt Handles
+/////////////////////////
 
+static irqreturn_t cc2520_sfdHandler(int irq, void *dev_id) 
+{
+    printk(KERN_INFO "[CC2520] - sfd interrupt occurred");
+    return IRQ_HANDLED;
+}
 
-
+static irqreturn_t cc2520_fifopHandler(int irq, void *dev_id) 
+{
+    printk(KERN_INFO "[CC2520] - fifop interrupt occurred");
+    return IRQ_HANDLED;
+}
 
 static enum hrtimer_restart callbackFunc(struct hrtimer *timer)
 {
@@ -35,37 +48,137 @@ static enum hrtimer_restart callbackFunc(struct hrtimer *timer)
     return HRTIMER_RESTART;
 }
 
+//////////////////////////////
+// Interface Initialization
+//////////////////////////////
 
-static irqreturn_t handler(int irq, void *dev_id) 
+// Sets up the GPIO pins needed for the CC2520
+// and initializes any interrupt handlers needed.
+int cc2520_setupGpioPins()
 {
-    gpio_set_value(23, pinValue == 1);
-    pinValue = !pinValue;
-    printk(KERN_INFO "TIMER INTERRUPTED %d\n", pinValue);
-    return IRQ_HANDLED;
+    int err = 0;
+    int irq = 0;
+
+    // Setup GPIO In/Out
+    err = gpio_request_one(CC2520_FIFO, GPIOF_DIR_IN, NULL);
+    if (err)
+        goto fail;
+
+    err = gpio_request_one(CC2520_FIFOP, GPIOF_DIR_IN, NULL);
+    if (err)
+        goto fail;
+
+    err = gpio_request_one(CC2520_CCA, GPIOF_DIR_IN, NULL);
+    if (err)
+        goto fail;
+
+    err = gpio_request_one(CC2520_SFD, GPIOF_DIR_IN, NULL);
+    if (err)
+        goto fail;
+
+    err = gpio_request_one(CC2520_DEBUG_0, GPIOF_DIR_OUT, NULL);
+    if (err)
+        goto fail;
+
+    err = gpio_request_one(CC2520_DEBUG_1, GPIOF_DIR_OUT, NULL);
+    if (err)
+        goto fail;
+
+    gpio_set_value(CC2520_DEBUG_0, 0);
+    gpio_set_value(CC2520_DEBUG_1, 0);
+
+    // Setup FIFOP Interrupt
+    irq = gpio_to_irq(CC2520_FIFOP);
+    if (irq < 0) {
+        err = irq;
+        goto fail;
+    }
+
+    err = request_irq(
+        irq, 
+        cc2520_fifopHandler, 
+        IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING, 
+        "fifopHandler", 
+        NULL
+    );
+    if (err)
+        goto fail;
+    state.gpios.fifopIrq = irq;
+
+
+    irq = gpio_to_irq(CC2520_SFD);
+    if (irq < 0) {
+        err = irq;
+        goto fail;
+    }
+
+    // Setup SFD Interrupt
+    err = request_irq(
+        irq, 
+        cc2520_sfdHandler, 
+        IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING, 
+        "sfdHandler", 
+        NULL
+    );
+    if (err)
+        goto fail;
+    state.gpios.sfdIrq = irq;
+
+    return err;
+
+    fail:
+        printk(KERN_INFO "Failed to init GPIOs\n");
+        cc2520_freeGpioPins();
+        return err;
 }
+
+void cc2520_freeGpioPins()
+{
+    gpio_free(CC2520_CLOCK);
+    gpio_free(CC2520_FIFO);
+    gpio_free(CC2520_FIFOP);
+    gpio_free(CC2520_CCA);
+    gpio_free(CC2520_SFD);
+
+    gpio_free(CC2520_DEBUG_0);
+    gpio_free(CC2520_DEBUG_1);
+
+    free_irq(state.gpios.fifopIrq, NULL);
+    free_irq(state.gpios.sfdIrq, NULL);
+}
+
+//////////////////////////////
+// Module Init/Cleanup
+//////////////////////////////
 
 int init_module()
 {
     ktime_t kt;
     int err = 0;
 
-    printk(KERN_INFO "Hello world 1.\n");
+    printk(KERN_INFO "Loading CC2520 Kernel Module v0.01...\n");
 
-    //////////////////////////
-    // GPIO Interrupt Init
-    err = gpio_request_one(22, GPIOF_DIR_IN, NULL);
-    err = gpio_request_one(23, GPIOF_DIR_OUT, NULL);
-    printk(KERN_INFO "Requesting GPIO Pin 22: %d\n", err);
-
-    irqNumber = gpio_to_irq(22);
-    printk(KERN_INFO "Requesting IRQ: %d\n", irqNumber);
-
-    if (irqNumber < 0) {
-        printk("Unable to get irq number.");
+    err = cc2520_setupGpioPins();
+    if (err) {
+        printk(KERN_INFO "[CC2520] - Error setting up GPIO pins. Aborting.");
         return 1;
     }
 
-    request_irq(irqNumber, handler, IRQF_TRIGGER_FALLING, "theInterrupt", NULL);
+    //////////////////////////
+    // GPIO Interrupt Init
+    //err = gpio_request_one(22, GPIOF_DIR_IN, NULL);
+    //err = gpio_request_one(23, GPIOF_DIR_OUT, NULL);
+    //printk(KERN_INFO "Requesting GPIO Pin 22: %d\n", err);
+
+    //irqNumber = gpio_to_irq(22);
+    //printk(KERN_INFO "Requesting IRQ: %d\n", irqNumber);
+
+    //if (irqNumber < 0) {
+    //    printk("Unable to get irq number.");
+    //    return 1;
+    //}
+
+    //request_irq(irqNumber, handler, IRQF_TRIGGER_FALLING, "theInterrupt", NULL);
 
     ////////////////////////
     // HFTimer Test
@@ -91,12 +204,9 @@ int init_module()
 
 void cleanup_module()
 {
-    free_irq(irqNumber, NULL);
-    gpio_free(22);
-    gpio_free(23);
+    cc2520_freeGpioPins();
     hrtimer_cancel(&utimer); //
-
-    printk(KERN_INFO "See ya.\n");
+    printk(KERN_INFO "Unloading CC2520 Kernel Module...\n");
 }
 
 MODULE_LICENSE("GPL");
--- cc2520.h
+++ cc2520.h
@@ -5,13 +5,16 @@
 // Physical mapping of GPIO pins on the CC2520
 // to GPIO pins on the linux microcontroller.
 
-#define CC2520_GPIO_0 0
-#define CC2520_GPIO_1 1
-#define CC2520_GPIO_2 2
-#define CC2520_GPIO_3 3
-#define CC2520_GPIO_4 4
-#define CC2520_GPIO_5 5
+#define CC2520_GPIO_0 -1
+#define CC2520_GPIO_1 25
+#define CC2520_GPIO_2 24
+#define CC2520_GPIO_3 22
+#define CC2520_GPIO_4 23
+#define CC2520_GPIO_5 -1
 #define CC2520_RESET 17
+
+#define CC2520_DEBUG_0 21
+#define CC2520_DEBUG_1 18
 
 // Logical mapping of CC2520 GPIO pins to
 // functions, we're going to keep these static
@@ -39,3 +42,16 @@
 //////////////////////////////
 // Structs and definitions
 /////////////////////////////
+
+struct cc2520_gpioState {
+	unsigned int fifopIrq;
+	unsigned int sfdIrq;
+};
+
+struct cc2520_state {
+	struct cc2520_gpioState gpios;
+};
+
+int cc2520_setupGpioPins(void);
+
+void cc2520_freeGpioPins(void);
